8b9ddd84f2ab90e5c103d0d533ac35b8284c7e92
Daniel Stuart <dstuart85@yahoo.com>
2022-06-08 20:48:28 -0400

    fixes bug

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,11 +5,13 @@ import { FaGithub } from "react-icons/fa"
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer__logo"></a>
+      <a href="#home" className="footer__logo">
+        DannyDevs
+      </a>
 
       <ul className="permalinks">
         <li>
-          <a href="#">Home</a>
+          <a href="#home">Home</a>
         </li>
         <li>
           <a href="#about">About</a>
