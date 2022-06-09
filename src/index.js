77cd403fd78378754b8008cd8a89a9088a3eee44
Daniel Stuart <dstuart85@yahoo.com>
2022-06-08 21:52:07 -0400

    attempting to fix heroku deploy bug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import ReactDOM from "react-dom"
+import React from "react"
+import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
 
-ReactDOM.render(<App />, document.querySelector("#root"))
+// ReactDOM.render(<App />, document.querySelector("#root"))
+
+const container = document.getElementById("root")
+const rootContainer = ReactDOM.createRoot(container)
+rootContainer.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
